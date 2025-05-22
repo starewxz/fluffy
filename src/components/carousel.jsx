9c81fcfa680f5d5250837ec43b4141b4d2d5bdc4
Stanislav Revasevych <revasevicstanislav@gmail.com>
2025-05-22 18:58:08 +0300

    change styles of carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -5,38 +5,45 @@ function Carousel() {
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src={`${import.meta.env.BASE_URL}img/dog-img.JPG`} className="d-block w-100" alt="Dog Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/dog-img.JPG`} className="d-block w-100"
+                         alt="Dog Slippers"/>
                 </div>
                 <div className="carousel-item">
-                    <img src={`${import.meta.env.BASE_URL}img/unicorn-img.JPG`} className="d-block w-100" alt="Unicorn Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/unicorn-img.JPG`} className="d-block w-100"
+                         alt="Unicorn Slippers"/>
                 </div>
                 <div className="carousel-item">
-                    <img src={`${import.meta.env.BASE_URL}img/cow-img.JPG`} className="d-block w-100" alt="Cow Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/cow-img.JPG`} className="d-block w-100"
+                         alt="Cow Slippers"/>
                 </div>
                 <div className="carousel-item">
-                    <img src={`${import.meta.env.BASE_URL}img/giraffe-img.JPG`} className="d-block w-100" alt="Giraffe Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/giraffe-img.JPG`} className="d-block w-100"
+                         alt="Giraffe Slippers"/>
                 </div>
                 <div className="carousel-item">
-                    <img src={`${import.meta.env.BASE_URL}img/goat-img.JPG`} className="d-block w-100" alt="Goat Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/goat-img.JPG`} className="d-block w-100"
+                         alt="Goat Slippers"/>
                 </div>
                 <div className="carousel-item">
-                    <img src={`${import.meta.env.BASE_URL}img/bear-img.JPG`} className="d-block w-100" alt="Bear Slippers"/>
+                    <img src={`${import.meta.env.BASE_URL}img/bear-img.JPG`} className="d-block w-100"
+                         alt="Bear Slippers"/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
                     data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="carousel-prev-icon" aria-hidden="true"><img
+                    src={`${import.meta.env.BASE_URL}img/right-arrow.png`} style={{transform: 'scaleX(-1)'}}/></span>
                 <span className="visually-hidden">Previous</span>
             </button>
             <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
                     data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="carousel-next-icon" aria-hidden="true"><img
+                    src={`${import.meta.env.BASE_URL}img/right-arrow.png`}/></span>
                 <span className="visually-hidden">Next</span>
             </button>
         </div>
     )
 }
-
 
 
 export default Carousel;
