51d87dc148274e1f373793572d77d7263148998d
Stanislav Revasevych <revasevicstanislav@gmail.com>
2025-06-01 22:14:34 +0300

    change architecture of project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? '/fluffy/' : '/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
