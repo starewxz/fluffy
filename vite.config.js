496080d3772db3c39d6cef7986572abbf90f50ff
Stanislav Revasevych <revasevicstanislav@gmail.com>
2025-06-01 22:05:37 +0300

    change architecture of project

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: isGitHubPages ? '/fluffy/' : '/',
   build: {
-    chunkSizeWarningLimit: 1000, // Increase warning limit (optional)
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            return "vendor"; // Moves dependencies to a separate file
+            return "vendor";
           }
         },
       },
     },
   },
   esbuild: {
-    jsxInject: `import React from 'react'`, // Ensures JSX is handled
+    jsxInject: `import React from 'react'`,
     jsxFactory: "React.createElement",
     jsxFragment: "React.Fragment",
   },
